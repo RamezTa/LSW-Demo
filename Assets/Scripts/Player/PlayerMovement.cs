b33ba714eb05f62fe00c469fe3ea25c365192b4c
Ramezov <ramez_magni@hotmail.com>
2021-06-12 20:54:58 +0300

    Some particle systems & foot steps SFX

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -5,7 +5,12 @@ using UnityEngine;
 [RequireComponent( typeof( PlayerAnimation ) )]
 public class PlayerMovement : MonoBehaviour
 {
+
     PlayerAnimation playerAnimation;
+    AudioSource audioSource;
+
+    public bool isPlayingSFX = false;
+
 
 
     [SerializeField] float movementSpeed;
@@ -16,6 +21,7 @@ public class PlayerMovement : MonoBehaviour
     void Awake()
     {
         playerAnimation = GetComponent<PlayerAnimation>();
+        audioSource = GetComponent<AudioSource>();
     }
 
     void Update()
@@ -34,6 +40,17 @@ public class PlayerMovement : MonoBehaviour
             transform.position + movementDirection,
             movementSpeed * Time.deltaTime
         );
+
+        if( (H!=0 || V!=0) && isPlayingSFX== false)
+        {
+            audioSource.Play();
+            isPlayingSFX = true;
+        }
+        else if ((H == 0 && V == 0)&& isPlayingSFX == true)
+        {
+            audioSource.Pause();
+            isPlayingSFX = false;
+        }
     }
 
 
